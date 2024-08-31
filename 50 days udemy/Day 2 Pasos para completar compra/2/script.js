a190216b6e872710db90315a3948b5cee9fa1ad7
RuxCastillo <rubenldc1412@gmail.com>
2024-08-31 11:55:20 -0600

    lo que trabaje en el camion

--- 50 days udemy/Day 2 Pasos para completar compra/2/script.js
+++ 50 days udemy/Day 2 Pasos para completar compra/2/script.js
@@ -12,12 +12,16 @@ function botonAnterior() {
     if(contador === 2) {
         contador--
         anterior.classList.add("block")
+        poniendoActive()
+        return
+    } else if (contador < 2) {
         return
     }
     contador--
     siguiente.classList.remove("block")
     anterior.classList.remove("block")
 
+    poniendoActive()
     console.log(contador)
 }
 
@@ -25,19 +29,37 @@ function botonSiguiente() {
     if(contador === 3) {
         contador++
         siguiente.classList.add("block")
+        poniendoActive()
+        return
+    } else if (contador > 3) {
         return
     }
     contador++
     anterior.classList.remove("block")
     siguiente.classList.remove("block")
+    poniendoActive()
 
     console.log(contador)
 }
 
 function poniendoActive() {
 
+    numbers.forEach((x) => {
+        x.classList.remove("active")
+    })
     
+    for(let i = 0; i < contador; i++) {
+        numbers[i].classList.add("active")
+    }
+
+    coloreandoLinea()
 
 }
 
+function coloreandoLinea() {
+    let lineAfter = document.querySelector(".barra-llena")
+    let linea = contador - 1;
+    let porcentaje = (33 * linea) + "%"
+    lineAfter.style.width = porcentaje;
+}
 
