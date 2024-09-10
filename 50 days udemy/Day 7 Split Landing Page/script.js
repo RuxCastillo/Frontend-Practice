96238ebe8289b5be5175a9b790e3979f867feb72
RuxCastillo <rubenldc1412@gmail.com>
2024-09-10 17:41:09 -0600

    dia 7 completado el primer intento, hacer mas de este

--- 50 days udemy/Day 7 Split Landing Page/script.js
+++ 50 days udemy/Day 7 Split Landing Page/script.js
@@ -1,3 +1,12 @@
+const play = document.querySelector('#play');
+const xbox = document.querySelector('#xbox');
+const container = document.querySelector('body');
 
-console.log("hola")
+function agrandar(consol) {
+	console.log(consol);
+	container.classList = '';
+	container.classList.add(consol);
+}
 
+play.addEventListener('mouseenter', () => agrandar('left'));
+xbox.addEventListener('mouseenter', () => agrandar('right'));
