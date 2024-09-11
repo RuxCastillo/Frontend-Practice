e70e636f440d80a583c6a9353c959096b35397f1
RuxCastillo <rubenldc1412@gmail.com>
2024-09-10 19:18:57 -0600

    tengo que mejorar en mis transiciones ya que no se porque una no quedo

--- 50 days udemy/Day 8 Form Wave Animation/script.js
+++ 50 days udemy/Day 8 Form Wave Animation/script.js
@@ -1,3 +1,23 @@
+console.log('hola');
 
-console.log("hola")
+const controlForm = document.querySelectorAll('.control-form');
+const label = document.querySelectorAll('label');
 
+controlForm[0].addEventListener('click', moviendoLetras);
+
+function moviendoLetras() {
+	label.forEach((letters) => {
+		let letra = letters.innerText.split('');
+		console.log(letra);
+		let string;
+		let contador = 0;
+		string = letra.map((l) => {
+			contador++;
+			return `<span style="transition-delay:${contador * 100}ms;">${l}</span>`;
+		});
+		console.log(string);
+		string = string.join('');
+		console.log(string);
+		letters.innerHTML = string;
+	});
+}
