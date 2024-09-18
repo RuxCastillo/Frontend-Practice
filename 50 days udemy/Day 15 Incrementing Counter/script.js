cdd4a362d57452c49152f438a2643f59384a3e7d
RuxCastillo <rubenldc1412@gmail.com>
2024-09-18 16:13:27 -0600

    ya termine el dia 15 de los contadores

--- 50 days udemy/Day 15 Incrementing Counter/script.js
+++ 50 days udemy/Day 15 Incrementing Counter/script.js
@@ -1,3 +1,21 @@
+const counters = document.querySelectorAll('.counter');
 
-console.log("hola")
+counters.forEach((count) => {
+	count.innerText = '0';
 
+	const updateCounter = () => {
+		const target = +count.getAttribute('data-target');
+		const c = +count.innerText;
+
+		const increment = target / 200;
+
+		if (c < target) {
+			count.innerText = `${Math.ceil(c + increment)}`;
+			setTimeout(updateCounter, 1);
+		} else {
+			count.innerText = target;
+		}
+	};
+
+	updateCounter();
+});
