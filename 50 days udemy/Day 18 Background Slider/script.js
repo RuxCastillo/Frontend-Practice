bc35fd7b16790b138a9df91a3bd63a6b5a124743
RuxCastillo <rubenldc1412@gmail.com>
2024-09-20 16:07:37 -0600

    ya quedo terminado el dia 18 de los 50 proyectos, es una galeria de imagenes

--- 50 days udemy/Day 18 Background Slider/script.js
+++ 50 days udemy/Day 18 Background Slider/script.js
@@ -1,3 +1,40 @@
+const nextButton = document.querySelector('.right');
+const prevButton = document.querySelector('.left');
+const body = document.querySelector('body');
+const pics = document.querySelectorAll('.pic');
 
-console.log("hola")
+let activeSlide = 0;
 
+setBackground();
+
+function setBackground() {
+	body.style.backgroundImage = pics[activeSlide].style.backgroundImage;
+}
+
+function activeSlideClass() {
+	pics.forEach((element) => element.classList.remove('active'));
+	pics[activeSlide].classList.add('active');
+}
+
+function next() {
+	activeSlide++;
+
+	if (activeSlide >= pics.length) {
+		activeSlide = 0;
+	}
+
+	activeSlideClass();
+	setBackground();
+}
+
+function prev() {
+	activeSlide--;
+	if (activeSlide < 0) {
+		activeSlide = pics.length - 1;
+	}
+	activeSlideClass();
+	setBackground();
+}
+
+nextButton.addEventListener('click', next);
+prevButton.addEventListener('click', prev);
