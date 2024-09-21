c9c8729b79390d41be47d75fc8a0ed74f14c329f
RuxCastillo <rubenldc1412@gmail.com>
2024-09-21 14:46:34 -0600

    acabado el dia 20 el ripple effect button aprendi mucho la verdad en este y fue muy sencillo

--- 50 days udemy/Day 20 Button Ripple Effect/script.js
+++ 50 days udemy/Day 20 Button Ripple Effect/script.js
@@ -1,3 +1,23 @@
+const button = document.querySelector('button');
 
-console.log("hola")
+button.addEventListener('click', function (event) {
+	const x = event.clientX;
+	const y = event.clientY;
 
+	const topButton = event.target.offsetTop;
+	const leftButton = event.target.offsetLeft;
+
+	const paraElCeroX = x - leftButton;
+	const paraElCeroY = y - topButton;
+
+	let elSpan = document.createElement('span');
+	elSpan.classList.add('circle');
+	elSpan.style.top = paraElCeroY + 'px';
+	elSpan.style.left = paraElCeroX + 'px';
+
+	this.appendChild(elSpan);
+
+	setInterval(() => {
+		elSpan.remove();
+	}, 1000);
+});
