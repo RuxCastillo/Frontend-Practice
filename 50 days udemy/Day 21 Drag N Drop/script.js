ab1bc96a85c9d3335c0da72997797bf701affcdf
RuxCastillo <rubenldc1412@gmail.com>
2024-09-22 19:52:19 -0600

    acabados proyecto 21, 22 y 23, llevo 47% del curso

--- 50 days udemy/Day 21 Drag N Drop/script.js
+++ 50 days udemy/Day 21 Drag N Drop/script.js
@@ -1,3 +1,42 @@
+const fill = document.querySelector('.fill');
+const empties = document.querySelectorAll('.empty');
 
-console.log("hola")
+fill.addEventListener('dragstart', dragStart);
+fill.addEventListener('dragend', dragEnd);
 
+for (let element of empties) {
+	element.addEventListener('dragover', dragOver);
+	element.addEventListener('dragenter', dragEnter);
+	element.addEventListener('dragleave', dragLeave);
+	element.addEventListener('drop', dragDrop);
+}
+
+function dragStart() {
+	console.log('drag start');
+	this.className += ' hold';
+	setTimeout(() => {
+		this.className = 'invisible';
+	}, 0);
+}
+function dragEnd() {
+	console.log('drag end');
+	this.className = 'fill';
+}
+function dragOver(e) {
+	e.preventDefault();
+	console.log('drag over');
+}
+function dragEnter(e) {
+	e.preventDefault();
+	console.log('drag enter');
+	this.classList += ' holdover';
+}
+function dragLeave() {
+	console.log('drag leave');
+	this.classList = 'empty';
+}
+function dragDrop() {
+	console.log('drag drop');
+	this.className = 'empty';
+	this.append(fill);
+}
--- 50 days udemy/Day 22 Drawing App/script.js
+++ 50 days udemy/Day 22 Drawing App/script.js
@@ -1,3 +1,87 @@
+const canvas = document.querySelector('#canvas');
+const ctx = canvas.getContext('2d');
+const menos = document.querySelector('.menos');
+const mas = document.querySelector('.mas');
+const tamañoEl = document.querySelector('.tamaño');
+const colorEl = document.querySelector('.color');
+const X = document.querySelector('.limpiar');
 
-console.log("hola")
+let tamaño = 10;
+let isPressed = false;
+colorEl.value = 'black';
+let color = colorEl.value;
+let x;
+let y;
 
+canvas.addEventListener('mousedown', (e) => {
+	isPressed = true;
+
+	x = e.offsetX;
+	y = e.offsetY;
+});
+
+canvas.addEventListener('mouseup', (e) => {
+	isPressed = false;
+
+	x = undefined;
+	y = undefined;
+});
+
+canvas.addEventListener('mousemove', (e) => {
+	if (isPressed) {
+		const x2 = e.offsetX;
+		const y2 = e.offsetY;
+
+		drawCircle(x2, y2);
+		drawLine(x, y, x2, y2);
+
+		x = x2;
+		y = y2;
+	}
+});
+
+function drawCircle(x, y) {
+	ctx.beginPath();
+	ctx.arc(x, y, tamaño, 0, Math.PI * 2);
+	ctx.fillStyle = color;
+	ctx.fill();
+}
+
+function drawLine(x1, y1, x2, y2) {
+	ctx.beginPath();
+	ctx.moveTo(x1, y1);
+	ctx.lineTo(x2, y2);
+	ctx.strokeStyle = color;
+	ctx.lineWidth = tamaño * 2;
+	ctx.stroke();
+}
+
+function cambiarTamaño() {
+	tamañoEl.innerText = tamaño;
+}
+
+mas.addEventListener('click', () => {
+	tamaño += 5;
+
+	if (tamaño > 50) {
+		tamaño = 50;
+	}
+
+	cambiarTamaño();
+});
+
+menos.addEventListener('click', () => {
+	tamaño -= 5;
+
+	if (tamaño < 5) {
+		tamaño = 5;
+	}
+
+	cambiarTamaño();
+});
+
+colorEl.addEventListener('change', (e) => (color = e.target.value));
+
+X.addEventListener('click', () =>
+	ctx.clearRect(0, 0, canvas.width, canvas.height)
+);
