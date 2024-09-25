8f08fe58c89568d93cf04b7ec3b4b1515b8dc3e0
RuxCastillo <rubenldc1412@gmail.com>
2024-09-25 16:09:12 -0600

    sticky nav bar completado el dia 25

--- 50 days udemy/Day 25 Sticky Navbar/script.js
+++ 50 days udemy/Day 25 Sticky Navbar/script.js
@@ -1,3 +1,12 @@
+const nav = document.querySelector('nav');
 
-console.log("hola")
+window.addEventListener('scroll', navChange);
 
+function navChange() {
+	console.log(window.scrollY, nav.offsetHeight);
+	if (window.scrollY > nav.offsetHeight + 150) {
+		nav.classList.add('active');
+	} else {
+		nav.classList.remove('active');
+	}
+}
