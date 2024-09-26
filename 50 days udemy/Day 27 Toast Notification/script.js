891ac90e554f2ddfe6dfbf5930caac1764c2e4f8
RuxCastillo <rubenldc1412@gmail.com>
2024-09-26 14:31:45 -0600

    acabado el proyecto del dia 27 toast notifications esto lo voy a agregar a mi portafolio sio si

--- 50 days udemy/Day 27 Toast Notification/script.js
+++ 50 days udemy/Day 27 Toast Notification/script.js
@@ -1,3 +1,25 @@
+const button = document.querySelector('.btn');
+const toast = document.querySelector('#toast');
 
-console.log("hola")
+const messages = [
+	'mensaje uno',
+	'mensaje dos',
+	'mensaje tres',
+	'mensaje cuatro',
+];
 
+button.addEventListener('click', () => {
+	const notificacion = document.createElement('div');
+	notificacion.classList.add('toast');
+
+	notificacion.innerText = randomMensaje();
+	toast.appendChild(notificacion);
+
+	setTimeout(() => {
+		notificacion.remove();
+	}, 3000);
+});
+
+function randomMensaje() {
+	return messages[Math.floor(Math.random() * messages.length)];
+}
