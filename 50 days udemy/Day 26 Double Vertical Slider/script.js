15900d12fab1c253146b560369cc6ea93594b2ed
RuxCastillo <rubenldc1412@gmail.com>
2024-09-25 22:30:54 -0600

    acabado dia 26 slides vertical in different direction

--- 50 days udemy/Day 26 Double Vertical Slider/script.js
+++ 50 days udemy/Day 26 Double Vertical Slider/script.js
@@ -1,3 +1,31 @@
+const container = document.querySelector('.container');
+const right = document.querySelector('.right');
+const left = document.querySelector('.left');
+const down = document.querySelector('.down');
+const up = document.querySelector('.up');
+const slidesLength = document.querySelectorAll('.right div').length;
 
-console.log("hola")
+let activeSlide = 0;
 
+left.style.top = `-${(slidesLength - 1) * 100}vh`;
+
+up.addEventListener('click', () => movimiento('up'));
+down.addEventListener('click', () => movimiento('down'));
+
+function movimiento(x) {
+	const heigthClient = container.clientHeight;
+	if (x === 'up') {
+		activeSlide++;
+		if (activeSlide > slidesLength - 1) {
+			activeSlide = 0;
+		}
+	} else if (x === 'down') {
+		activeSlide--;
+		if (activeSlide < 0) {
+			activeSlide = slidesLength - 1;
+		}
+	}
+
+	left.style.transform = `translateY(${heigthClient * activeSlide}px)`;
+	right.style.transform = `translateY(-${heigthClient * activeSlide}px)`;
+}
