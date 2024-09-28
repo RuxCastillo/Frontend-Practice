630bbf8a5f470a82fb36863ed07989c57c2ac3e1
RuxCastillo <rubenldc1412@gmail.com>
2024-09-27 20:56:42 -0600

    acabado el dia 32 los checkboxes

--- 50 days udemy/Day 32 Good, Cheap, Fast Checkboxes/script.js
+++ 50 days udemy/Day 32 Good, Cheap, Fast Checkboxes/script.js
@@ -1,3 +1,22 @@
+const toggles = document.querySelectorAll('.toggle');
+const cheap = document.querySelector('#cheap');
+const good = document.querySelector('#good');
+const fast = document.querySelector('#fast');
 
-console.log("hola")
+toggles.forEach((toggle) => {
+	toggle.addEventListener('change', (e) => doTheTrick(e.target));
+});
 
+function doTheTrick(clicked) {
+	if (good.checked && cheap.checked && fast.checked) {
+		if (good === clicked) {
+			fast.checked = false;
+		}
+		if (cheap === clicked) {
+			good.checked = false;
+		}
+		if (fast === clicked) {
+			cheap.checked = false;
+		}
+	}
+}
