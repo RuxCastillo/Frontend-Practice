bb0a10b1f0f839ffe02289ad7203315a0cb1f267
RuxCastillo <rubenldc1412@gmail.com>
2024-10-01 16:00:03 -0600

    acabado el dia 38 la nav bar del celular

--- 50 days udemy/Day 38 Mobile Tab Navigation/script.js
+++ 50 days udemy/Day 38 Mobile Tab Navigation/script.js
@@ -1,3 +1,20 @@
+const contents = document.querySelectorAll('.content');
+const listItems = document.querySelectorAll('nav ul li');
 
-console.log("hola")
+listItems.forEach((item, idx) => {
+	item.addEventListener('click', () => {
+		hideAllContent();
+		hideAllItems();
 
+		item.classList.add('active');
+		contents[idx].classList.add('show');
+	});
+});
+
+function hideAllContent() {
+	contents.forEach((content) => content.classList.remove('show'));
+}
+
+function hideAllItems() {
+	listItems.forEach((item) => item.classList.remove('active'));
+}
