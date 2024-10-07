c7b26d17561377cf7d2a8017a4c53f6c2e4c9b94
RuxCastillo <rubenldc1412@gmail.com>
2024-10-07 14:35:44 -0600

    acabado el dia 48 y 49

--- 50 days udemy/Day 48 Random Image Feed/script.js
+++ 50 days udemy/Day 48 Random Image Feed/script.js
@@ -1,3 +1,17 @@
+const container = document.querySelector('.container');
+const unsplashURL = 'https://source.unsplash.com/random/';
+const rows = 10;
 
-console.log("hola")
+for (let i = 0; i < rows * 3; i++) {
+	const img = document.createElement('img');
+	img.src = `${unsplashURL}${getRandomSize()}`;
+	container.appendChild(img);
+}
 
+function getRandomNr() {
+	return Math.floor(Math.random() * 10) + 300;
+}
+
+function getRandomSize() {
+	return `${getRandomNr()}x${getRandomNr()}`;
+}
