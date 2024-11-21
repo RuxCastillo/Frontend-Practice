216ca726e2910c82b34f6c0c195330a78a120e00
RuxCastillo <rubenldc1412@gmail.com>
2024-11-21 12:57:44 -0600

    continuo con vanilla js frontend master

--- frontendmasters/Vanilla Js you might not need a framework/coffeemasters-vanilla-main/coffeemasters-vanilla-main/components/MenuPage.js
+++ frontendmasters/Vanilla Js you might not need a framework/coffeemasters-vanilla-main/coffeemasters-vanilla-main/components/MenuPage.js
@@ -8,7 +8,7 @@ export class MenuPage extends HTMLElement {
 		this.root.appendChild(styles);
 
 		async function loadCSS() {
-			const request = await fetch('/components/MenuPage.css');
+			const request = await fetch('./components/MenuPage.css');
 			const css = await request.text();
 			styles.textContent = css;
 		}
@@ -19,6 +19,31 @@ export class MenuPage extends HTMLElement {
 		const template = document.getElementById('menu-page-template');
 		const content = template.content.cloneNode(true);
 		this.root.appendChild(content);
+
+		window.addEventListener('appmenuchange', () => {
+			this.render();
+		});
+	}
+	render() {
+		if (app.store.menu) {
+			this.root.querySelector('#menu').innerHTML = '';
+			for (let category of app.store.menu) {
+				const liCategory = document.createElement('li');
+				liCategory.innerHTML = `
+				<h3>${category.name}</h3>
+				<ul class='category'></ul>
+				`;
+				this.root.querySelector('#menu').appendChild(liCategory);
+
+				category.products.forEach((product) => {
+					const item = document.createElement('product-item');
+					item.dataset.product = JSON.stringify(product);
+					liCategory.querySelector('ul').appendChild(item);
+				});
+			}
+		} else {
+			this.root.querySelector('#menu').innerHTML = 'Loading...';
+		}
 	}
 }
 customElements.define('menu-page', MenuPage);
